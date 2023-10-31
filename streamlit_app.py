1ca60bbc4e30cee41121f0736037fbec8a297c66
mohsims <148848542+mohsims@users.noreply.github.com>
2023-10-31 10:58:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-streamlit.header('Moms Healthy Diner')
+streamlit.title('My Mom's Healthy Diner')
 streamlit.header('🥣Breakfast Menu')
 streamlit.text('🥗Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥑🍞Kale, Spinach & Rocket Smoothie')
